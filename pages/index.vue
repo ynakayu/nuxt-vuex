a01f045ce7219ef9cb1ac5efff0929185ebc9a63
ynakayu <9111754+ynakayu@users.noreply.github.com>
2019-06-27 15:16:46 +0900

    actions

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <div>
       <p>{{ $store.state.message }}</p>
       <!-- <button v-on:click="$store.commit('updateMessage')">Update</button> -->
-      <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button>
+      <!-- <button v-on:click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button v-on:click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+      <button v-on:click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button>
+
     </div>
   </section>
 </template>
